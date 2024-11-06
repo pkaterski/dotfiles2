a377911a54a3906a5e271f341ff5691e8943f8b2
Parashkev Katerski <pkaterski@gmail.com>
2024-11-06 12:26:38 +0200

    jk also esc in term

--- nvim/lua/pesho/remap.lua
+++ nvim/lua/pesho/remap.lua
@@ -6,3 +6,4 @@ vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode' })
 
 -- for terminal escaping
 vim.keymap.set('t', '<ESC>', "<C-\\><C-n><C-w>h",{silent = true})
+vim.keymap.set('t', 'jk', "<C-\\><C-n><C-w>h",{silent = true})

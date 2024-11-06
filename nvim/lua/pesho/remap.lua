264000cc343e23cc2346b90212e0582449341ef5
Parashkev Katerski <pkaterski@gmail.com>
2024-11-06 12:16:09 +0200

    escape term

--- nvim/lua/pesho/remap.lua
+++ nvim/lua/pesho/remap.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode' })
+
+-- for terminal escaping
+vim.keymap.set('t', '<ESC>', "<C-\\><C-n><C-w>h",{silent = true})
